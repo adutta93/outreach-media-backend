70b9d5242160d0f87f2435955a8f93b04e7fd670
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2022-03-30 22:12:16 +0530

    make it better

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-//   next();
-// });
+  next();
+});
 app.use(errorHandler);
 
 // conncet database
