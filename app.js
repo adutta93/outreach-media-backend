6ceaae0c068dca0f4a9574f20d8e342046d5a530
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2022-03-30 22:20:53 +0530

    make it better

--- app.js
+++ app.js
@@ -18,12 +18,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
   );
-
   next();
 });
 app.use(errorHandler);
