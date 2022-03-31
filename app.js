58748dabb9fa12adc140c34e3b7f0e241cad1a2c
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2022-03-31 20:23:13 +0530

    cors policy added

--- app.js
+++ app.js
@@ -8,28 +8,28 @@ require("dotenv").config();
 const { errorHandler } = require("./middleware/errorHandler");
 
 //middleware
+app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
   })
 );
 app.use(express.json({ extended: false }));
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   //Enabling CORS
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+//   );
+//   next();
+// });
 app.use(errorHandler);
 
 // conncet database
